4d1ad8dff90b86082791d4d5eb6d351ae62197cd
Starlight19973 <starlight1997@yandex.ru>
2024-02-27 16:29:23 +0700

    env

--- rishat/rishat/settings.py
+++ rishat/rishat/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ve!r2!a8k8n+qmqr*21m7#pi78x35z+w!eqh(npgi5_87q1m07'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
